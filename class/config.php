83a399aa0c052ef359488f81d23c8e70282c77e5
nolight132 <contact@nolight.dev>
2026-03-28 19:59:47 +0100

    env vars

--- class/config.php
+++ class/config.php
@@ -1,15 +1,15 @@
 <?php
-define('SKIN_LANGUAGE', 'skins_en');
+define('SKIN_LANGUAGE', getenv('SKIN_LANGUAGE') ?: 'skins_en');
 
-define('DB_HOST', 'CHANGE_ME');
-define('DB_PORT', '3306');
-define('DB_NAME', 'CHANGE_ME');
-define('DB_USER', 'CHANGE_ME');
-define('DB_PASS', 'CHANGE_ME');
+define('DB_HOST', getenv('DB_HOST') ?: 'cs2-mysql');
+define('DB_PORT', getenv('DB_PORT') ?: '3306');
+define('DB_NAME', getenv('DB_NAME') ?: '');
+define('DB_USER', getenv('DB_USER') ?: '');
+define('DB_PASS', getenv('DB_PASS') ?: '');
 
-define('WEB_STYLE_DARK', true);
+define('WEB_STYLE_DARK', filter_var(getenv('WEB_STYLE_DARK') ?: 'true', FILTER_VALIDATE_BOOLEAN));
 
-define('STEAM_API_KEY', 'CHANGE_ME');
-define('STEAM_DOMAIN_NAME', 'CHANGE_ME');
-define('STEAM_LOGOUT_PAGE', '');
-define('STEAM_LOGIN_PAGE', '');
+define('STEAM_API_KEY', getenv('STEAM_API_KEY') ?: '');
+define('STEAM_DOMAIN_NAME', getenv('STEAM_DOMAIN_NAME') ?: '');
+define('STEAM_LOGOUT_PAGE', getenv('STEAM_LOGOUT_PAGE') ?: '');
+define('STEAM_LOGIN_PAGE', getenv('STEAM_LOGIN_PAGE') ?: '');
